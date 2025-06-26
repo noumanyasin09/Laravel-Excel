7a2bd7a3fbb7e102c5e09444b8944f666e4df206
Nouman Yasin <41565800+noumanyasin09@users.noreply.github.com>
2025-06-26 10:27:24 +0300

    update chunk size to 100000

--- src/Sheet.php
+++ src/Sheet.php
@@ -672,7 +672,7 @@ class Sheet
             return ArrayHelper::ensureMultipleRows(
                 static::mapArraybleRow($row)
             );
-        })->chunk(20000)->each(function ($rows) use ($sheetExport) {
+        })->chunk(100000)->each(function ($rows) use ($sheetExport) {
             $this->append(
                 $rows->toArray(),
                 $sheetExport instanceof WithCustomStartCell ? $sheetExport->startCell() : null,
